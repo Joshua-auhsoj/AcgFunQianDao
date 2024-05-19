e611c355195cff155aaa61658133c3b4e0c9f392
Joshua <137018415+Joshua-auhsoj@users.noreply.github.com>
2024-05-19 21:14:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ import re
 import asyncio
 from telegram import Bot
 
+process_1 = subprocess.Popen(['python', 'DAILY.py'], stdout=subprocess.PIPE)
+output_1, _ = process_1.communicate()
+process_2 = subprocess.Popen(['python', 'BALANCE.py'], stdout=subprocess.PIPE)
+output_2, _ = process_2.communicate()
+
+
+response_text1 = output_1.decode()
+response_text2 = output_2.decode()
+
+
+
+print(response_text1)
+print(response_text1)
+
+
 bot_token = os.environ.get('BOTTOKEN')
 chat_id = os.environ.get('USERID')
 
 # 创建 Bot 实例
+
 bot = Bot(token=bot_token)
 
 
+# 发送消息
 async def send_message():
-    # 调用子进程并获取输出
-    try:
-        output_1 = subprocess.check_output(['python', 'DAILY.py']).decode()
-        output_2 = subprocess.check_output(['python', 'BALANCE.py']).decode()
-        botmessage =output_1 + output_2
-        print(botmessage)
-
-        # 发送消息
-        await bot.send_message(chat_id=chat_id, text=botmessage)
-
-    except subprocess.CalledProcessError as e:
-        print("子进程调用错误:", e)
-
-    except Exception as e:
-        print("发生其他异常:", e)
+    await bot.send_message(chat_id=chat_id, text=response_text1)
 
 
 # 运行异步函数
