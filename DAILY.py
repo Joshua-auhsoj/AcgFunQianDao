2cde6268ab1f8779702860fd2874c03fea24e93a
Joshua <137018415+Joshua-auhsoj@users.noreply.github.com>
2024-09-27 20:09:10 +0800

    Update DAILY.py

--- DAILY.py
+++ DAILY.py
@@ -1,62 +1,42 @@
 import requests
 import os
 import json
+import xml.etree.ElementTree as ET
 
-import cloudscraper
-
-scraper = cloudscraper.create_scraper()  # 创建一个scraper对象
-
-url = "https://rjhome.me/wp-json/b2/v1/getUserMission"
-
-
-
+# 获取 cookie 值
 cookie_value = os.getenv('COOKIE')
 cookies = {cookie.split('=')[0]: cookie.split('=')[1] for cookie in cookie_value.split('; ')}
 
-
-
-
-
+# 请求头
 headers = {
-    'accept': 'application/json, text/plain, */*',
-    'accept-language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcmpob21lLm1lIiwiaWF0IjoxNzE4MTc1MDAxLCJuYmYiOjE3MTgxNzUwMDEsImV4cCI6MTcxOTM4NDYwMSwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiODIxNDkifX19.z0e7fy-FLjpzxIjNJ514fGT4rWidc5KSNlrw6AmyRWc',
-    'content-type': 'application/x-www-form-urlencoded',
-    'dnt': '1',
-    'origin': 'https://rjhome.me',
-    'priority': 'u=1, i',
-    'referer': 'https://rjhome.me/message',
-    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'Accept': '*/*',
+    'Accept-Language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Referer': 'https://acgfun.moe/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
 }
 
-data = {
-    'count': '10',
-    'paged': '1',
-}
+# 发送请求
+response = requests.get(
+    'https://acgfun.moe/plugin.php?id=k_misign:sign&operation=qiandao&format=global_usernav_extra&formhash=b21898d7&inajax=1&ajaxtarget=k_misign_topb',
+    cookies=cookies,
+    headers=headers,
+)
 
-#response = requests.post('https://rjhome.me/wp-json/b2/v1/getUserMission', cookies=cookies, headers=headers, data=data)
-response = scraper.post(url, cookies=cookies, headers=headers, data=data)
+# 打印原始响应
+#print(response.text)
 
-json_data = json.loads(response.text)
+# 解析 XML 数据
+root = ET.fromstring(response.content)
+message = root.text.strip()
 
-# 提取所需的内容
-my_credit = json_data['mission']['my_credit']
-credit = json_data['mission']['credit']
-
-# 格式化输出
-output = f"签到积分: {credit}\n总积分: {my_credit}"
-
-# 打印结果
-print(output)
-
-
-
-
-
-
+# 输出签到结果
+print(f"签到状态: {message}")
